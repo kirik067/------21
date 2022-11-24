9ece479ae5e744ff9484689a41b50cfbce3adb2e
kirill <tvoag.2015@mail.ru>
2022-11-24 19:08:51 +0500

    19 21 23

---
+++ Program.cs
@@ -0,0 +1,68 @@
+﻿// задача 21
+/*
+int x1 = ReadInt("Введите координату X первой точки: ");
+int y1 = ReadInt("Введите координату Y первой точки: ");
+int z1 = ReadInt("Введите координату Z первой точки: ");
+int x2 = ReadInt("Введите координату X второй точки: ");
+int y2 = ReadInt("Введите координату Y второй точки: ");
+int z2 = ReadInt("Введите координату Z второй точки: ");
+
+int A = x2 - x1;
+int B = y2 - y1;
+int C = z1 - z2;
+
+double length = Math.Sqrt(A * A + B * B + C * C);
+Console.WriteLine($"Длинна отрезка {length}");
+
+int ReadInt(string message)
+{
+ Console.Write(message);
+ return Convert.ToInt32(Console.ReadLine());
+}
+
+// задача 19 
+/*
+void CheckPalindromicNumber(int number)
+{
+    if(number >= 10000)
+    {
+        int a = number / 10000;
+        int b = number % 10;
+ 
+            if(a == b)
+            {
+                number = number / 10;
+                int a1 = (number / 100) % 10;
+                int b2 = number % 10;
+                if(a1 == b2)
+                    Console.WriteLine("палиндромом");
+            }
+            else 
+            Console.WriteLine("Не палиндромом");
+            
+    }
+    else
+    Console.WriteLine("Некорректное число!");
+}
+ 
+Console.WriteLine("Введите пятизначное число:");
+int number = int.Parse(Console.ReadLine()!);
+CheckPalindromicNumber(number);
+
+//задача23
+/*
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{ 
+    Console.Write($"{i*i*i} ");
+}
+
+
+
+// Функция ввода сообщения
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
